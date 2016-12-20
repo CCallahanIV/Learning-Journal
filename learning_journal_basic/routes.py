d51890ad422deba67d0310f0b2df6059c58cf20a
Ted Callahan <CCallahanIV@gmail.com>
2016-12-19 17:28:21 -0800

    Wrote routes and views, updated README.md.

--- learning_journal_basic/routes.py
+++ learning_journal_basic/routes.py
@@ -1,5 +1,10 @@
+"""Routes for the Learning Journal app."""
+
+
 def includeme(config):
     """All of the routes for the configuration to find."""
     config.add_static_view('static', 'static', cache_max_age=3600)
     config.add_route("home", "/")
-    config.add_route("thing", "/stuff")
+    config.add_route("/journal/{id:\d+}", "detail")
+    config.add_route("/journal/new-entry", "create")
+    config.add_route("/journal/{id:\d+}/edit-entry", "update")
--- learning_journal_basic/views.py
+++ learning_journal_basic/views.py
@@ -1,3 +1,5 @@
+"""Views for the Pyramid Learning Journal app."""
+
 from pyramid.response import Response
 import io
 import os
@@ -7,10 +9,35 @@ HERE = os.path.dirname(__file__)
 
 def home_page(request):
     """View for the home page."""
-    file_path = os.path.join(HERE, "data", "sample.html")
+    file_path = os.path.join(HERE, "templates", "index.html")
+    file_data = io.open(file_path).read()
+    return Response(file_data)
+
+
+def detail_view(request):
+    """Handle the detail view for a specific journal entry."""
+    file_path = os.path.join(HERE, "templates", "entry.html")
+    file_data = io.open(file_path).read()
+    return Response(file_data)
+
+
+def create_view(request):
+    """Handle the view for creating a new entry."""
+    file_path = os.path.join(HERE, "templates", "create_entry.html")
+    file_data = io.open(file_path).read()
+    return Response(file_data)
+
+
+def update_view(request):
+    """Handle the view for updating a new entry."""
+    file_path = os.path.join(HERE, "templates", "edit_entry.html")
     file_data = io.open(file_path).read()
     return Response(file_data)
 
 
 def includeme(config):
+    """Set up views with routes."""
     config.add_view(home_page, route_name="home")
+    config.add_view(detail_view, route_name="detail")
+    config.add_view(create_view, route_name="create")
+    config.add_view(update_view, route_name="update")
